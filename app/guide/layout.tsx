ad5bf2ef481c1376a8e2f7be27338736c4bba425
hinosxz <gauthier.henon@datadoghq.com>
2024-04-24 10:28:40 -0400

    Fix header

--- app/guide/layout.tsx
+++ app/guide/layout.tsx
@@ -3,6 +3,7 @@ import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useMemo } from "react";
+import classNames from "classnames";
 
 export default function Layout({
   children,
@@ -17,9 +18,10 @@ export default function Layout({
       <div className="min-h-screen">
         <header>
           <div
-            className={
-              "flex p-4 hover:opacity-75 cursor-pointer items-center gap-2"
-            }
+            className={classNames(
+              "w-fit flex p-4 items-center gap-2",
+              "hover:opacity-75 cursor-pointer"
+            )}
             onClick={() => router.back()}
           >
             <ArrowLeftIcon className="h-4" />
