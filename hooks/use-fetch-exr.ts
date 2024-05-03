3909f00ad20324f25d3bc50fcb47b56a7fb78096
Julien Muetton <julien@muetton.me>
2024-05-03 17:49:27 +0200

    Add a way to fetch multiple rates in a single hook

--- hooks/use-fetch-exr.ts
+++ hooks/use-fetch-exr.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { useMemo } from "react";
+import { ONE_DAY } from "@/lib/constants";
+import { UseQueryResult, useQueries, useQuery } from "@tanstack/react-query";
 
 const apiUrl =
   "https://data-api.ecb.europa.eu/service/data/EXR/D.USD.EUR.SP00.A";
@@ -26,42 +26,89 @@ export interface ExchangeRate {
   errorMessage: string | null;
 }
 
-export const useFetchExr = (date: string): ExchangeRate => {
-  const searchParams = useMemo(() => {
-    const p = new URLSearchParams({
-      format: "jsondata",
-      detail: "dataonly",
-    });
-    p.set("startPeriod", date);
-    p.set("endPeriod", date);
-    return p;
-  }, [date]);
+const fetchExchangeRate = async (date: string): Promise<number> => {
+  const searchParams = new URLSearchParams({
+    format: "jsondata",
+    detail: "dataonly",
+  });
+  searchParams.set("startPeriod", date);
+  searchParams.set("endPeriod", date);
 
-  const { data, isFetching, error } = useQuery({
+  return fetch(`${apiUrl}?${searchParams.toString()}`)
+    .then((res) => res.json())
+    .then(
+      (data: Response) =>
+        data.dataSets[0].series["0:0:0:0:0"].observations[0][0],
+    );
+};
+
+export const useFetchExr = (
+  date: string,
+): {
+  rate: number | null;
+  isFetching: boolean;
+  errorMessage: string | null;
+} => {
+  const {
+    data: rate,
+    isFetching,
+    error,
+  } = useQuery({
     queryKey: ["USD_EUR_EXCHANGE_RATE", date],
-    queryFn: (): Promise<Response> =>
-      fetch(`${apiUrl}?${searchParams.toString()}`).then((res) => res.json()),
+    queryFn: () => fetchExchangeRate(date),
     retry: 1,
+    staleTime: ONE_DAY,
   });
 
-  let rate: number | null = null;
-  let errorMessage: string | null = null;
-
-  if (error) {
-    errorMessage = `failed fetching exchange rate for date ${date}`;
-  }
-
-  try {
-    if (data) {
-      rate = data.dataSets[0].series["0:0:0:0:0"].observations[0][0];
-    }
-  } catch {
-    errorMessage = "failed extracting rate from api response";
-  }
-
   return {
-    rate,
+    rate: rate ?? null,
     isFetching,
-    errorMessage,
+    errorMessage: error ? "failed extracting rate from api response" : null,
   };
+};
+
+interface UseExchangeRateResponse {
+  isFetching: boolean;
+  responses: {
+    [date: string]: UseQueryResult<number, unknown>;
+  };
+  values: {
+    [date: string]: number;
+  };
+}
+const DEFAULT_EXCHANGE_RATE = 1;
+export const useExchangeRates = (dates: string[]): UseExchangeRateResponse => {
+  const results = useQueries({
+    queries: dates.map((date) => ({
+      queryKey: ["USD_EUR_EXCHANGE_RATE", date],
+      queryFn: async () => {
+        const rate = await fetchExchangeRate(date);
+        // To keep track of the date in combine, it is repeated in the result
+        return { date, rate };
+      },
+      staleTime: ONE_DAY,
+    })),
+  });
+
+  const data: UseExchangeRateResponse = {
+    isFetching: false,
+    responses: {},
+    values: {},
+  };
+
+  for (const { data: fetchResponse, ...result } of results) {
+    const query = {
+      ...result,
+      // Overwrite the data with the rate only
+      data: fetchResponse?.rate ?? DEFAULT_EXCHANGE_RATE,
+    } as UseQueryResult<number, unknown>;
+
+    data.isFetching = data.isFetching || result.isFetching;
+    if (fetchResponse) {
+      data.responses[fetchResponse.date] = query;
+      data.values[fetchResponse.date] = fetchResponse.rate;
+    }
+  }
+
+  return data;
 };
