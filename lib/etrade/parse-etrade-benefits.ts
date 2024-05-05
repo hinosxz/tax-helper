188dd9c0e2c71a7389a0ad0d1e13061eb1b1ddeb
Julien Muetton <julien@muetton.me>
2024-05-05 23:07:25 +0200

    Add stub for benefits file parsing

---
+++ lib/etrade/parse-etrade-benefits.ts
@@ -0,0 +1,10 @@
+import { BenefitHistoryEvent } from "./etrade.types";
+
+export const parseEtradeBenefits = (
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  file: File,
+): Promise<BenefitHistoryEvent[]> => {
+  // Parsing of benefits is a level higher than parsing of gains and losses.
+  // let's keep it for later.
+  throw new Error("Not implemented");
+};
