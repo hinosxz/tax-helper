2424afcd63cf8f4ef6b0a2c5921a7f4e49f282e7
Julien Muetton <julien@muetton.me>
2024-05-06 22:50:30 +0200

    Add TaxableEvent

---
+++ components/TaxableEventFr.tsx
@@ -0,0 +1,122 @@
+import { TaxableEventFr as TaxableEventFrProps } from "@/lib/taxes/taxable-event-fr";
+import { match } from "ts-pattern";
+import { Drawer } from "./ui/Drawer";
+import { Currency } from "@/app/guide/shared/ui/Currency";
+import { PriceInEuro } from "./ui/PriceInEuro";
+import { formatDateFr } from "@/lib/date";
+
+export const TaxableEventFr: React.FunctionComponent<{
+  event: TaxableEventFrProps;
+  showAcquisitionGains?: boolean;
+  showCapitalGains?: boolean;
+}> = ({ event, showCapitalGains = false, showAcquisitionGains = false }) => {
+  const asset = match(event.planType)
+    .with("ESPP", () => "ESPP")
+    .with("RS", () => "RSU")
+    .with("SO", () => "Stock options")
+    .exhaustive();
+
+  const trigger = match(event.type)
+    .with("vesting", () => "vested")
+    .with("sell", () => "sold")
+    .with("exercise", () => "exercised")
+    .exhaustive();
+
+  return (
+    <Drawer
+      title={
+        <div className="flex items-baseline justify-start gap-2">
+          <h2 className="font-bold text-lg">
+            On {formatDateFr(event.date)} {trigger} {event.quantity} {asset}
+          </h2>
+
+          <dl className="flex items-baseline justify-start gap-2">
+            {showCapitalGains && (
+              <>
+                <dt className="font-bold">Capital gain</dt>
+                <dd>
+                  <Currency value={event.capitalGain.total} unit="eur" />
+                </dd>
+              </>
+            )}
+            {showAcquisitionGains && (
+              <>
+                <dt className="font-bold">Acquisition gain</dt>
+                <dd>
+                  <Currency value={event.acquisitionGain.total} unit="eur" />
+                </dd>
+              </>
+            )}
+          </dl>
+        </div>
+      }
+    >
+      <TaxableEventFrLine title="Acquisition value">
+        <Drawer
+          title={
+            <>
+              <p>
+                <PriceInEuro
+                  eur={event.acquisition.valueEur}
+                  usd={event.acquisition.valueUsd}
+                  rate={event.acquisition.rate}
+                  date={event.acquisition.date}
+                />{" "}
+                per share ({event.acquisition.description})
+              </p>
+            </>
+          }
+        >
+          <TaxableEventFrLine title={`${event.symbol} price at opening:`}>
+            <PriceInEuro
+              eur={event.acquisition.symbolPriceEur}
+              usd={event.acquisition.symbolPrice}
+              rate={event.acquisition.rate}
+              date={event.acquisition.date}
+            />{" "}
+            per share.
+          </TaxableEventFrLine>
+        </Drawer>
+      </TaxableEventFrLine>
+      <TaxableEventFrLine title="Acquisition cost">
+        <PriceInEuro
+          eur={event.acquisition.costEur}
+          usd={event.acquisition.costUsd}
+          rate={event.acquisition.rate}
+          date={event.acquisition.date}
+        />{" "}
+        per share.
+      </TaxableEventFrLine>
+      {event.sell && (
+        <TaxableEventFrLine title="Sell">
+          <Currency value={event.sell.eur} unit="eur" /> per share on{" "}
+          {formatDateFr(event.sell.date)}
+        </TaxableEventFrLine>
+      )}
+      {showAcquisitionGains && (
+        <TaxableEventFrLine title="Acquisition gain">
+          <Currency value={event.acquisitionGain.perShare} unit="eur" /> per
+          share.
+        </TaxableEventFrLine>
+      )}
+      {showCapitalGains && (
+        <TaxableEventFrLine title="Capital gain">
+          <Currency value={event.capitalGain.perShare} unit="eur" /> per share.
+        </TaxableEventFrLine>
+      )}
+    </Drawer>
+  );
+};
+interface TaxableEventFrLineProps {
+  title: React.ReactNode;
+  children: React.ReactNode;
+}
+const TaxableEventFrLine: React.FunctionComponent<TaxableEventFrLineProps> = ({
+  title,
+  children,
+}) => (
+  <div className="flex items-start justify-stretch">
+    <h4 className="w-1/3">{title}</h4>
+    <div className="grow">{children}</div>
+  </div>
+);
