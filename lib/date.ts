cdd117395b794e0d93011b4f17b5e33f5298460d
Julien Muetton <julien@muetton.me>
2024-05-06 16:27:47 +0200

    Add a way to format FR date

--- lib/date.ts
+++ lib/date.ts
@@ -5,3 +5,25 @@ export const parseEtradeDate = (rawDate: string) => {
   const [month, day, year] = rawDate.split("/").map(Number);
   return new Date(Date.UTC(year, month - 1, day));
 };
+
+const months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
+/**
+ * Format a date to a French date.
+ */
+export const formatDateFr = (/** format YYYY-MM-DD */ date: string) => {
+  const [year, monthNumber, day] = date.split("-").map(Number);
+  return `${day} ${months[monthNumber]} ${year}`;
+};
