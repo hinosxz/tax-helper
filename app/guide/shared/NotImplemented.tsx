652b7db9249a7704afc9f4228f698bd2c7448448
Damien Pacaud <damien.pacaud@gmail.com>
2024-05-06 17:11:46 +0200

    Update app/guide/shared/NotImplemented.tsx
    Co-authored-by: Gauthier Hénon <33065180+hinosxz@users.noreply.github.com>
    Signed-off-by: Damien Pacaud <damien.pacaud@gmail.com>

--- app/guide/shared/NotImplemented.tsx
+++ app/guide/shared/NotImplemented.tsx
@@ -1,11 +1,12 @@
 import { Link } from "@/app/guide/shared/ui/Link";
 
+const GITHUB_REPO_URL = "https://github.com/hinosxz/tax-helper";
+  
 export default function NotImplemented() {
-  const githubRepoURL = "https://github.com/hinosxz/tax-helper";
   return (
     <div>
       not implemented yet, feel free to contribute to{" "}
-      <Link href={githubRepoURL} isExternal>
+      <Link href={GITHUB_REPO_URL} isExternal>
         {" "}
         the repository
       </Link>
