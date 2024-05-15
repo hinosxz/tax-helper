1ca814c75edf578ffb0217b51ff43752816e7f48
Julien Muetton <julien@muetton.me>
2024-05-15 21:00:07 +0200

    Use ts-pattern for report selection

--- app/report/Report.tsx
+++ app/report/Report.tsx
@@ -5,13 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import classNames from "classnames";
 import { ReportResidencyFrTaxesFr } from "./residency_fr_taxes_fr/ReportResidencyFrTaxesFr";
 import { Select } from "@/components/ui/Select";
+import { match } from "ts-pattern";
 
 export type CountryCode = "us" | "fr";
 export interface ReportProps {}
 
 export const Report: React.FunctionComponent<ReportProps> = () => {
-  const [taxesCountry, setTaxesCountry] = useState<Partial<CountryCode>>("fr");
-  const [residency, setResidencyCountry] = useState<Partial<CountryCode>>("fr");
+  const [taxesCountry, setTaxesCountry] = useState<CountryCode | null>("fr");
+  const [residency, setResidencyCountry] = useState<CountryCode | null>("fr");
 
   const queryClient = useMemo(() => new QueryClient(), []);
 
@@ -29,7 +30,7 @@ export const Report: React.FunctionComponent<ReportProps> = () => {
                 { label: "US", value: "us" },
                 { label: "FR", value: "fr" },
               ]}
-              value={residency}
+              value={residency ?? undefined}
             />
             <Select
               className="text-lg"
@@ -39,18 +40,21 @@ export const Report: React.FunctionComponent<ReportProps> = () => {
                 { label: "US", value: "us" },
                 { label: "FR", value: "fr" },
               ]}
-              value={taxesCountry}
+              value={taxesCountry ?? undefined}
             />
           </div>
         </header>
         <main className="flex flex-col items-center justify-around p-24">
-          {!residency || !taxesCountry ? (
-            <p>Please select a residency and taxes country</p>
-          ) : residency === "fr" && taxesCountry === "fr" ? (
-            <ReportResidencyFrTaxesFr />
-          ) : (
-            <p>Not yet implemented</p>
-          )}
+          {match({ residency, taxesCountry })
+            .with({ residency: null, taxesCountry: null }, () => (
+              <p>Please select a residency and taxes country</p>
+            ))
+            .with({ residency: "fr", taxesCountry: "fr" }, () => (
+              <ReportResidencyFrTaxesFr />
+            ))
+            .otherwise(() => (
+              <p>Not yet implemented</p>
+            ))}
         </main>
       </div>
     </QueryClientProvider>
