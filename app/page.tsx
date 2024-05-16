642de9af1af12599a6f2f466846cfc0791079db8
Julien Muetton <julien@muetton.me>
2024-05-16 10:05:44 +0200

    Add report as an option on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,26 @@ export default function Home() {
       )}
     >
       <header className="text-4xl font-semibold">Tax Helper</header>
-      <Link
-        className={`
+      <div className="flex flex-col w-96 mx-auto items-stretch text-center gap-10">
+        <Link
+          className={`
           bg-green-200 px-12 py-4 rounded shadow 
           hover:opacity-75 text-base font-semibold
         `}
-        href="/guide"
-      >
-        Start
-      </Link>
+          href="/guide"
+        >
+          Guide
+        </Link>
+        <Link
+          className={`
+          bg-green-200 px-12 py-4 rounded shadow 
+          hover:opacity-75 text-base font-semibold
+        `}
+          href="/report"
+        >
+          Compute my tax report
+        </Link>
+      </div>
     </main>
   );
 }
