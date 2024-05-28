7d5eb4d317b0f6c6b87bbf9e6081654141a12269
hinosxz <gauthier.henon@datadoghq.com>
2024-05-28 17:57:58 +0200

    Create `Modal` component

---
+++ components/ui/Modal.tsx
@@ -0,0 +1,22 @@
+import classNames from "classnames";
+import { ReactNode } from "react";
+
+interface ModalProps {
+  children: ReactNode;
+  show: boolean;
+}
+
+export const Modal = ({ show, children }: ModalProps) => {
+  return (
+    <div
+      tabIndex={-1}
+      className={classNames("fixed z-50", "overflow-y-auto overflow-x-hidden", {
+        hidden: !show,
+      })}
+    >
+      <div className="relative p-4 bg-gray-100 rounded shadow w-full">
+        {children}
+      </div>
+    </div>
+  );
+};
