d7fbe999f48953382d3647f0da3a0b89deb879dd
hinosxz <gauthier.henon@icloud.com>
2024-06-09 09:57:40 -0400

    Hide % of French origin if 100%

--- components/TaxableEventFr.tsx
+++ components/TaxableEventFr.tsx
@@ -90,11 +90,13 @@ export const TaxableEventFr: React.FunctionComponent<{
         />{" "}
         per share ({event.acquisition.description})
       </TaxableEventFrLine>
-      <TaxableEventFrLine title="% of French Origin">
-        <span className="font-semibold">
-          {(event.acquisitionGain.fractionFr * 100).toFixed(2)}%
-        </span>
-      </TaxableEventFrLine>
+      {event.acquisitionGain.fractionFr < 1 ? (
+        <TaxableEventFrLine title="% of French Origin">
+          <span className="font-semibold">
+            {(event.acquisitionGain.fractionFr * 100).toFixed(2)}%
+          </span>
+        </TaxableEventFrLine>
+      ) : null}
       {event.sell && (
         <TaxableEventFrLine title="Sell price">
           <PriceInEuro
