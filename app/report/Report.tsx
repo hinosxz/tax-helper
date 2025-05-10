41202b555a16716139b9585118d9772a95fbbb57
hinosxz <33065180+hinosxz@users.noreply.github.com>
2025-05-10 11:41:23 +0200

    Implement `ButtonGroup` component for switching residencies

--- app/report/Report.tsx
+++ app/report/Report.tsx
@@ -2,31 +2,29 @@
 import { useState } from "react";
 import classNames from "classnames";
 import { ReportResidencyFr } from "./_ReportResidencyFr";
-import { Select } from "@/components/ui/Select";
 import { match } from "ts-pattern";
 import NotImplemented from "@/components/NotImplemented";
+import type { Option } from "@/components/ui/ButtonGroup";
+import { ButtonGroup } from "@/components/ui/ButtonGroup";
 
 export type CountryCode = "us" | "fr";
+
 export interface ReportProps {}
 
 export const Report: React.FunctionComponent<ReportProps> = () => {
   const [taxResidency, setTaxResidency] = useState<CountryCode>("fr");
+  const options: Option<CountryCode>[] = [
+    { value: "fr", label: "FR" },
+    { value: "us", label: "US" },
+  ];
 
   return (
     <div>
       <div className={classNames("w-fit flex p-4 items-center gap-2")}>
         <div className="text-2xl font-bold">Tax Report</div>
-        <span>|</span>
-        <Select
-          className="text-lg"
-          label="Residency:"
-          onChange={setTaxResidency}
-          options={[
-            { label: "US", value: "us" },
-            { label: "FR", value: "fr" },
-          ]}
-          value={taxResidency}
-        />
+        <div className="inline-flex w-0.5 self-stretch bg-black"></div>
+        <div>Residency:</div>
+        <ButtonGroup onClick={setTaxResidency} options={options} />
       </div>
       <div>
         {match({ taxResidency })
